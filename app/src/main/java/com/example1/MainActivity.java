5299173480bdcd019c6da0e1a058846882c9cdd3
WindSekirun <windsekirun@live.co.kr>
2015-06-02 16:05:57 +0900

    commit

---
+++ app/src/main/java/com/example1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends ActionBarActivity {
+    Button button1;
+    EditText editText1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+        editText1 = (EditText) findViewById(R.id.editText);
+    }
+}
